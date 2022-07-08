dfaf115468bdcedb0f48563b65c02f696351ea49
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-07 21:46:14 -0300

    feat: iniciando comunicação com api TMDB

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+const movieUrl = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 function Home() {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    console.log(data)
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${movieUrl}top_rated?${apiKey}`
+
+    console.log(topRatedUrl)
+    getTopRatedMovies(topRatedUrl)
+
+  }, [])
+
   return (
     <div>
       Home
