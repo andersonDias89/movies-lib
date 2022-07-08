049ac8645939bd36fc4b34040d8c2ff42026fb93
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-08 18:12:59 -0300

    refactor: definindo layout dos cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom"
-import {FaStar} from 'react-icons/fa'
+import { FaStar } from 'react-icons/fa'
 
 const imageUrl = import.meta.env.VITE_IMG
 
 export function MovieCard({ movie, showLink = true }) {
     return (
-        <div>
-             <img src={imageUrl + movie.poster_path} alt={movie.title} />
-             <h2>{movie.title}</h2>
-             <p>
-                <FaStar/> {movie.vote_average}
-             </p>
-             {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+        <div className="p-2 bg-gray-700 w-72 mt-5">
+            <div className="w-full">
+                <img src={imageUrl + movie.poster_path} alt={movie.title} />
+                <div className="flex justify-between py-2">
+                    <h2>{movie.title}</h2>
+                    <p className="flex items-center space-x-1">
+                        <span><FaStar /></span> 
+                        <span>{movie.vote_average}</span>
+                    </p>
+                </div>
+                <div className="w-full">
+                    {showLink && <Link to={`/movie/${movie.id}`}>
+                        <button type="button" className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Detalhes</button>
+                    </Link>}
+                </div>
+            </div>
         </div>
     )
 }
