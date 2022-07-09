7cebdeca866bf189f3f4823c45f898348d4afcfe
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-09 01:05:40 -0300

    feat: criando função para capturar o onchange do input search e direcionar a busca para a rota

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom';
 import { Logo } from "./Logo";
 
 export function Navbar() {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
+    function handleSearch(e) {
+        e.preventDefault()
+        console.log(search)
+
+        if (!search) return 
+
+        navigate(`/search?q=${search}`)
+        setSearch('')
+
+    }
+
     return (
         <div className="w-full h-20 degrade flex items-center">
             <div className="container flex items-center justify-between">
                 <Logo />
-                <form className="flex justify-end">
+                <form
+                    onSubmit={handleSearch}
+                    className="flex justify-end"
+                >
                     <div className="flex bg-transparent border border-card p-2 rounded-md justify-between">
                         <input
+                            onChange={e => setSearch(e.target.value)}
+                            value={search}
                             type="text"
                             placeholder='Search'
                             className=" bg-transparent placeholder:text-slate-500 border-none formSearch w-20 text-white outline-none"
