1037f20e04b88141055a62e8f3062e2ed1bfbeda
Anderson Dias <andersondiasmd25@gmail.com>
2022-07-09 10:50:44 -0300

    feat: listando filmes de acordo com a busca do formulário com o searchUrl da API

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import {MovieCard} from '../components/MovieCard'
+
+const serachUrl = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
 function Search() {
+  const [searchParams] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+
+  const query = searchParams.get("q")
+
+  const getSeachedMovies = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setMovies(data.results)
+    console.log(movies) 
+  }
+
+  useEffect(() => {
+    const searchWithQueryUrl = `${serachUrl}?${apiKey}&query=${query}`
+
+    getSeachedMovies(searchWithQueryUrl)
+  }, [query])
+
     return (
-      <div>
-        Search
+      <div className="w-full my-6">
+      <div className="container grid grid-cols-3 gap-12">
+        {movies.length > 0 &&
+          movies.map(movie => {
+            return (
+              <MovieCard key={movie.id} movie={movie} />
+            )
+          })}
       </div>
+    </div>
     )
   }
 
